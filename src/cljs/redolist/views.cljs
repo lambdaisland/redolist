00ee9c293c3293d53180424b7cea20f14eee62b7
Arne Brasseur <arne@arnebrasseur.net>
2017-02-06 12:23:04 +0100

    003 - Implement the first subscription and views

--- src/cljs/redolist/views.cljs
+++ src/cljs/redolist/views.cljs
@@ -1,7 +1,34 @@
 (ns redolist.views
-    (:require [re-frame.core :as re-frame]))
+  (:require [re-frame.core :as re-frame :refer [subscribe]]
+            [redolist.helpers :refer [class->]]))
+
+(defn todo-checkbox [id completed]
+  [:input.toggle {:type "checkbox"
+                  :checked completed}])
+
+(defn todo-item [todo]
+  (fn [{:keys [id completed title editing] :as todo}]
+    [:li {:class (class-> completed "completed "
+                          editing   "editing")}
+     [:div.view
+      [todo-checkbox id completed]
+      [:label
+       {:unselectable "on"
+        :style {:user-select "none"
+                :-moz-user-select "none"}}
+       title]
+      [:button.destroy {}]]]))
+
+(defn todo-list []
+  (let [todos (subscribe [:visible-todos])]
+    (fn []
+      [:div#todo-list
+       (for [todo @todos]
+         ^{:key (:id todo)} [todo-item todo])])))
 
 (defn main-panel []
-  (let [name (re-frame/subscribe [:name])]
-    (fn []
-      [:div "Hello from " @name])))
+  [:div
+   [:section#todoapp
+    [:div
+     [:section#main
+      [todo-list]]]]])
