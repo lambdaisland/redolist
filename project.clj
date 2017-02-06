3b3ee66331990226fb8fde42cf4bd8c4c82c0d3d
Arne Brasseur <arne@arnebrasseur.net>
2017-02-06 11:23:21 +0100

    Add all necessary dependencies

--- project.clj
+++ project.clj
@@ -1,23 +1,25 @@
 (defproject redolist "0.1.0-SNAPSHOT"
   :dependencies [[org.clojure/clojure "1.9.0-alpha14"]
                  [org.clojure/clojurescript "1.9.456"]
+
+                 ;; Frontend
                  [cljsjs/react-with-addons "15.4.2-2"]
                  [cljsjs/react-dom "15.4.2-2"]
                  [cljsjs/react-dom-server "15.4.2-2"]
-                 [reagent "0.6.0" :exclusions [cljsjs/react
-                                               cljsjs/react-dom
-                                               cljsjs/react-dom-server]]
+                 [reagent "0.6.0" :exclusions [cljsjs/react cljsjs/react-dom cljsjs/react-dom-server]]
                  [re-frame "0.9.1"]
                  [secretary "1.2.3"]
+                 [cljs-ajax "0.5.8"]
                  [garden "1.3.2"]
+
+                 ;; Backend
                  [com.stuartsierra/component "0.3.2"]
                  [org.danielsz/system "0.4.0"]
                  [compojure "1.5.2"]
                  [ring/ring-defaults "0.2.2"]
                  [ring-middleware-format "0.7.2"]
                  [ring "1.5.1"]
-                 [spicerack "0.1.1"]
-                 [cljs-ajax "0.5.8"]]
+                 [spicerack "0.1.1"]]
 
   :plugins [[lein-cljsbuild "1.1.4"]
             [lein-garden "0.2.8"]]
@@ -40,8 +42,12 @@
   :profiles
   {:dev
    {:dependencies [[binaryage/devtools "0.9.0"]
+
+                   ;; For cider-jack-in-clojurescript
                    [figwheel-sidecar "0.5.9"]
                    [com.cemerick/piggieback "0.2.1"]
+
+                   ;; dependencies for the reloaded workflow
                    [lambdaisland/garden-watcher "0.3.0"]
                    [reloaded.repl "0.2.3"]
                    [ns-tracker "0.3.1"]]
